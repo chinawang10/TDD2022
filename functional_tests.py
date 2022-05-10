b528502ca864fa20e31bcc6f0125db05b17403b1
Wang Jinfeng <wjf@bupt.edu.cn>
2022-05-10 13:42:31 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase): #(1)
         self.assertIn('To-Do',header_text)
         
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.fin_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -37,8 +37,8 @@ class NewVisitorTest(unittest.TestCase): #(1)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table = self.browser.fin_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
             )
