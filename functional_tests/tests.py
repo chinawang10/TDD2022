7e999e19cf0e7771640b66998a4923664b06c35b
Wang Jinfeng <wjf@bupt.edu.cn>
2022-06-02 15:55:32 +0800

    new URL + view for adding to existing lists. FT passes :D

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -61,9 +61,9 @@ class NewVisitorTest(LiveServerTestCase): #(1)
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
        
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # The page updates again, and now shows both items on her list      
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- ther is some
