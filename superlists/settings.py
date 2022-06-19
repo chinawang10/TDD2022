ba21079903b2a76ef1764cd0cac17ff8010ed277
Wang Jinfeng <wjf@bupt.edu.cn>
2022-06-19 15:52:31 +0800

    Add gunicorn to virtualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hd=j#v*sd@g-@po_p&av83a1dfd!=17_w)czeq9^epv&n((d4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['106.14.115.184']
 
 
 # Application definition
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 
 # Default primary key field type
